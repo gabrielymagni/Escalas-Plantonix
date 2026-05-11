e889833ac9af5da36d8bf35183b473a4955933af
Gabriely <gabrielymagni@gmail.com>
2026-05-11 11:14:43 -0300

    refact #27: condição de não ter escalas ainda, não premite edição

--- front/src/pages/private/Services/Escalas/GerarEscala/components/TabelaEscala.jsx
+++ front/src/pages/private/Services/Escalas/GerarEscala/components/TabelaEscala.jsx
@@ -61,23 +61,30 @@ const TabelaEscala = ({ dadosEscala, handleChange, submitSalvar }) => {
                             : "transparent",
                         fontWeight: ehHoje ? "bold" : "normal"
                     }}>
-                    <Select size="small" variant="standard" disableUnderline
-                        disabled={buttonEditar}
-                        value={getTurno(profissional, dataFormatada)}
-                        onChange={(e) =>
-                            handleChange(profissional.id, dataFormatada, e.target.value)
-                        }
-                        sx={{
-                            color: "#000", borderRadius: 2, p: 0.5,
-                            bgcolor: background(getTurno(profissional, dataFormatada))
-                        }}
-                    >
-                        {OpcoesEventos.map(op => (
-                            <MenuItem key={op.id} value={op.id}>
-                                {op.id}
-                            </MenuItem>
-                        ))}
-                    </Select>
+                    {getTurno(profissional, dataFormatada) ? (
+                        <Select size="small" variant="standard" disableUnderline
+                            disabled={buttonEditar}
+                            value={getTurno(profissional, dataFormatada)}
+                            onChange={(e) =>
+                                handleChange(profissional.id, dataFormatada, e.target.value)
+                            }
+                            sx={{
+                                color: "#000", borderRadius: 2, p: 0.5,
+                                bgcolor: background(getTurno(profissional, dataFormatada))
+                            }}
+                        >
+                            {OpcoesEventos.map(op => (
+                                <MenuItem key={op.id} value={op.id}>
+                                    {op.id}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    ) : (
+                        <>
+                            -
+                        </>
+                    )
+                    }
                 </TableCell>
             );
         });
@@ -85,82 +92,82 @@ const TabelaEscala = ({ dadosEscala, handleChange, submitSalvar }) => {
 
     return (
         <Paper sx={{ p: 2, border: '2px solid #50b5ae' }}>
-                <Typography sx={{
-                    fontSize: '15px', color: buttonEditar ? '#000' : '#b61b1b',
-                    textAlign: 'start',
-                }}>
-                    * Tabela em modo de {buttonEditar ? 'visualização' : 'edição'}
-                </Typography>
+            <Typography sx={{
+                fontSize: '15px', color: buttonEditar ? '#000' : '#b61b1b',
+                textAlign: 'start',
+            }}>
+                * Tabela em modo de {buttonEditar ? 'visualização' : 'edição'}
+            </Typography>
 
-                <ButtonsPrevNext
-                    handleNext={proximoMes}
-                    handlePrev={mesAnterior}
-                    periodo={formatarPeriodo(inicio, diasNoPeriodo)}
-                    liberaEditar={liberaEditar}
-                />
+            <ButtonsPrevNext
+                handleNext={proximoMes}
+                handlePrev={mesAnterior}
+                periodo={formatarPeriodo(inicio, diasNoPeriodo)}
+                liberaEditar={liberaEditar}
+            />
 
-                <TableContainer sx={{ mt: 2 }}>
-                    <Table size="small">
+            <TableContainer sx={{ mt: 2 }}>
+                <Table size="small">
 
-                        <TableHead>
-                            <TableRow >
+                    <TableHead>
+                        <TableRow >
+                            <TableCell sx={{
+                                border: '1px solid #50b5ae', p: 0.5,
+                                position: "sticky", left: 0, bgcolor: "#f3f3f3", zIndex: 3
+                            }}>
+                                <b>Profissional</b>
+                            </TableCell>
+
+                            {renderDiasHeader()}
+                        </TableRow>
+                    </TableHead>
+
+                    <TableBody>
+                        {dadosEscala.map((prof) => (
+                            <TableRow key={prof.id} sx={{ p: 0 }}>
                                 <TableCell sx={{
                                     border: '1px solid #50b5ae', p: 0.5,
-                                    position: "sticky", left: 0, bgcolor: "#f3f3f3", zIndex: 3
+                                    position: "sticky",
+                                    left: 0,
+                                    bgcolor: "#f3f3f3",
+                                    zIndex: 2
                                 }}>
-                                    <b>Profissional</b>
+                                    {prof.nome}
                                 </TableCell>
 
-                                {renderDiasHeader()}
+                                {renderCelulas(prof)}
+
                             </TableRow>
-                        </TableHead>
+                        ))}
+                    </TableBody>
 
-                        <TableBody>
-                            {dadosEscala.map((prof) => (
-                                <TableRow key={prof.id} sx={{ p: 0 }}>
-                                    <TableCell sx={{
-                                        border: '1px solid #50b5ae', p: 0.5,
-                                        position: "sticky",
-                                        left: 0,
-                                        bgcolor: "#f3f3f3",
-                                        zIndex: 2
-                                    }}>
-                                        {prof.nome}
-                                    </TableCell>
+                </Table>
+            </TableContainer>
 
-                                    {renderCelulas(prof)}
+            {!buttonEditar &&
+                <Grid container spacing={2} sx={{
+                    display: 'flex', justifyContent: 'center', alignItems: 'center', mt: 3
+                }}>
 
-                                </TableRow>
-                            ))}
-                        </TableBody>
-
-                    </Table>
-                </TableContainer>
-
-                {!buttonEditar &&
-                    <Grid container spacing={2} sx={{
-                        display: 'flex', justifyContent: 'center', alignItems: 'center', mt: 3
+                    <Grid size={{ md: 6, xs: 12 }} sx={{
+                        display: 'flex', justifyContent: 'center', alignItems: 'center',
                     }}>
-
-                        <Grid size={{ md: 6, xs: 12 }} sx={{
-                            display: 'flex', justifyContent: 'center', alignItems: 'center',
-                        }}>
-                            <Button onClick={submitSalvar}
-                                sx={{
-                                    bgcolor: '#258119', color: '#fff',
-                                    transition: 'all 0.3s ease',
-                                    '&:hover': {
-                                        transform: 'translateY(-3px)',
-                                    }
-                                }} endIcon={<CheckCircleIcon />}>
-                                Salvar alterações
-                            </Button>
-                        </Grid>
-
+                        <Button onClick={submitSalvar}
+                            sx={{
+                                bgcolor: '#258119', color: '#fff',
+                                transition: 'all 0.3s ease',
+                                '&:hover': {
+                                    transform: 'translateY(-3px)',
+                                }
+                            }} endIcon={<CheckCircleIcon />}>
+                            Salvar alterações
+                        </Button>
                     </Grid>
-                }
 
-                <Toaster />
+                </Grid>
+            }
+
+            <Toaster />
         </Paper>
 
     )
