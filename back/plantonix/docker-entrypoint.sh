49fc8edb705c69b5fc188caba62ada628e7983b8
Ruan Vasconcelos <ruanvasconcelos898@gmail.com>
2026-06-01 13:44:01 -0300

    chore: add db seed to docker entrypoint

--- back/plantonix/docker-entrypoint.sh
+++ back/plantonix/docker-entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 echo "Rodando migrations..."
 php artisan migrate --force
 
+echo "Rodando seeders..."
+php artisan db:seed --force
+
 echo "Cacheando configurações..."
 php artisan config:cache
 php artisan route:cache
